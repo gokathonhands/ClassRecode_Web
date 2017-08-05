70e9a81cdbf825f137bcee500b2839f30c1547f8
ChoiHanJin <zxa018@korea.ac.kr>
2017-08-05 22:53:54 +0900

    10:53

--- real/static/js/app.js
+++ real/static/js/app.js
@@ -2,9 +2,8 @@
 
 var record = document.querySelector('.record');
 var stop = document.querySelector('.stop');
-var soundClips = document.querySelector('.sound-clips');
-var canvas = document.querySelector('.visualizer');
-var mainSection = document.querySelector('.main-controls');
+var soundClips = document.getElementsByClassName(".sound-clips");
+console.log(soundClips);
 
 // disable stop button while not recording
 
@@ -13,7 +12,6 @@ stop.disabled = true;
 // visualiser setup - create web audio api context and canvas
 
 var audioCtx = new (window.AudioContext || webkitAudioContext)();
-var canvasCtx = canvas.getContext("2d");
 
 //main block for doing the audio recording
 
@@ -23,40 +21,44 @@ if (navigator.mediaDevices.getUserMedia) {
     var constraints = { audio: true };
     var chunks = [];
 
+    var clipDown = document.createElement('a');
+    
     var onSuccess = function(stream) {
         var mediaRecorder = new MediaRecorder(stream);
-
-        visualize(stream);
-
+/**
+ *  record click
+ */
         record.onclick = function() {
             mediaRecorder.start();
             console.log(mediaRecorder.state);
             console.log("recorder started");
             record.style.background = "red";
-
+            
             stop.disabled = false;
             record.disabled = true;
         }
-
+/**
+ *  stop click
+ */
         stop.onclick = function() {
             mediaRecorder.stop();
             console.log(mediaRecorder.state);
             console.log("recorder stopped");
             record.style.background = "";
             record.style.color = "";
-            // mediaRecorder.requestData();
 
             stop.disabled = true;
             record.disabled = false;
         }
-
+/**
+ *  record stop
+ */
         mediaRecorder.onstop = function(e) {
             console.log("data available after MediaRecorder.stop() called.");
 
             var clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
             console.log(clipName);
             var clipContainer = document.createElement('article');
-            var clipDown = document.createElement('a');
             var audio = document.createElement('audio');
             var deleteButton = document.createElement('button');
 
@@ -131,63 +133,8 @@ if (navigator.mediaDevices.getUserMedia) {
 } else {
     console.log('getUserMedia not supported on your browser!');
 }
-/*
-function visualize(stream) {
-  var source = audioCtx.createMediaStreamSource(stream);
-
-  var analyser = audioCtx.createAnalyser();
-  analyser.fftSize = 2048;
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
-
-  source.connect(analyser);
-  //analyser.connect(audioCtx.destination);
-
-  draw()
-
-  function draw() {
-    WIDTH = canvas.width
-    HEIGHT = canvas.height;
-
-    requestAnimationFrame(draw);
-
-    analyser.getByteTimeDomainData(dataArray);
-
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-    canvasCtx.beginPath();
-
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
-    var x = 0;
 
 
-    for(var i = 0; i < bufferLength; i++) {
- 
-      var v = dataArray[i] / 128.0;
-      var y = v * HEIGHT/2;
-
-      if(i === 0) {
-        canvasCtx.moveTo(x, y);
-      } else {
-        canvasCtx.lineTo(x, y);
-      }
-
-      x += sliceWidth;
-    }
-
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
-
-  }
-}
-
-window.onresize = function() {
-  canvas.width = mainSection.offsetWidth;
-}
-
-window.onresize();
-*/
+$(document).ready(function() {
+    
+});
